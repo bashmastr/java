512fcc5575b4a72d4051c5c16afebdf64867c741
123ahmad <bashirahmad7242@gmail.com>
2018-06-28 09:00:31 +0000

    Constructor

---
+++ Animal04.java
@@ -0,0 +1,15 @@
+
+public class Animal04{
+	///////constructor//////
+	public Animal04(){
+
+		System.out.println("Animal class created ..");
+
+	}
+
+	public static void main(String args[]){
+		System.out.println("Main function call");
+
+		Animal04 a = new Animal04();
+	}
+}
